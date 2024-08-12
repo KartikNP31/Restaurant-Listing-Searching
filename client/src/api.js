d44fce0e5a5370194d78ca1fc6b025dd155394f7
KartikNP31 <patelkn1303@gmail.com>
2024-08-12 07:01:13 +0530

    update deploy

--- client/src/api.js
+++ client/src/api.js
@@ -1,1 +1,2 @@
-export const PUBLIC_SERVER_URL="http://localhost:8000"
+export const PUBLIC_SERVER_URL="https://zomato-restaurant-listing-searching.onrender.com"
+// export const PUBLIC_SERVER_URL="http://localhost:8000"
