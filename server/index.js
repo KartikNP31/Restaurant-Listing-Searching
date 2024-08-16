6aa214f33537582139a90f96e93a4d528c430440
Kartik Patel <patelkn1303@gmail.com>
2024-08-16 18:42:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,10 +8,9 @@ const app = express();
 const port = 8000;
 
 mongoose.connect(process.env.mongoURI
-  // , {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true
-  // }
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000,
 ).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
